ffbc3e44370c81ff46947c368d1ec126f51e0013
KC_software_en <kisha.cairncross@gmail.com>
2025-01-21 23:54:50 +0200

    added load_dotenv in PayPal/utils.py to access content in .env file

--- PayPal/utils.py
+++ PayPal/utils.py
@@ -4,6 +4,11 @@ import paypalrestsdk
 # import os to access environment variables
 import os
 
+# import load_dotenv to load environment variables from .env file
+import os
+from dotenv import load_dotenv
+
+
 ##############################################################################################
 
 # configure paypal sdk
@@ -13,16 +18,24 @@ def configure_paypal():
     :return: Return the configured PayPal SDK.
     :rtype: paypalrestsdk.Api
     """
-    # get the client ID and client secret from environment variables
-    client_id = os.getenv('PAYPAL_CLIENT_ID')
+    # load environment variables from .env file
+    # call load_dotenv method to read the .env file & make the content available to os.getenv
+    load_dotenv()
+
+    # get the client ID, client secret & PayPal mode from environment variables
+    client_id = os.getenv('PAYPAL_CLIENT_ID')    
     client_secret = os.getenv('PAYPAL_CLIENT_SECRET')
+    print(client_id)
+    print(client_secret)
+    paypal_mode = os.getenv('PAYPAL_MODE')
+    print(paypal_mode)
 
     # configure the PayPal SDK with the client ID and client secret
     paypalrestsdk.configure({
-        "mode": "sandbox",  # sandbox or live
+        "mode": 'sandbox',#paypal_mode,  # sandbox or live
         "client_id": client_id,
         "client_secret": client_secret
     })
 
     # return the configured PayPal SDK
-    return paypalrestsdk.Api
+    ##return paypalrestsdk.Api ## might remove
