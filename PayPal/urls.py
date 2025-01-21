915358e313d809c963cc24f995ccb6f9898b5cec
KC_software_en <kisha.cairncross@gmail.com>
2025-01-21 13:56:58 +0200

    addedURLs for views in PayPal/urls.py

--- PayPal/urls.py
+++ PayPal/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 
+# add urls for views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='payment_page'),
+    path('create/', views.create_payment, name='create_payment'),    
+    path('success/', views.payment_success, name='payment_success'),    
     ]
