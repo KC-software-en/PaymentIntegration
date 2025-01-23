f1ac914308a332f274aeb5e39d459ea158e9b215
KC_software_en <kisha.cairncross@gmail.com>
2025-01-23 18:46:41 +0200

    added a status check test for index view in PayPal/tests.py

--- PayPal/tests.py
+++ PayPal/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
+# RequestFactory returns a request
+from django.test.client import RequestFactory
+
+# import the views to be tested
+from .views import *
+
+#############################################################################################
+
 # Create your tests here.
+
+# Test for the index view.
+class TestIndexView(TestCase):
+    # Create a setUp method to create a user which is logged in before each test.
+    def setUp(self):
+        # Create a RequestFactory instance.
+        self.factory = RequestFactory()        
+
+    def test_index_status(self):
+        # Make a request to the '/payment/' endpoint.
+        request = self.factory.get('/payment/')
+
+        # call the index view with a mock request response
+        response = index(request)
+
+        # Assert that the response is ok
+        self.assertEqual(response.status_code, 200)
