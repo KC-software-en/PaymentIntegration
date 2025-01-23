2c61067d4d7a733076f264433ebcbacfbb3085a7
KC_software_en <kisha.cairncross@gmail.com>
2025-01-23 19:58:13 +0200

    hardcoded sandbox mode in PayPal/utils.py because of github actions error ('Configuration Mode Invalid', 'Received: None', 'Required: live or sandbox')

--- PayPal/utils.py
+++ PayPal/utils.py
@@ -20,14 +20,13 @@ def configure_paypal():
     # call load_dotenv method to read the .env file & make the content available to os.getenv
     load_dotenv()
 
-    # get the client ID, client secret & PayPal mode from environment variables
+    # get the PayPal client ID & client secret from environment variables
     client_id = os.getenv('PAYPAL_CLIENT_ID')    
-    client_secret = os.getenv('PAYPAL_CLIENT_SECRET')    
-    paypal_mode = os.getenv('PAYPAL_MODE')    
+    client_secret = os.getenv('PAYPAL_CLIENT_SECRET')         
 
     # configure the PayPal SDK with the client ID and client secret
     paypalrestsdk.configure({
-        "mode": paypal_mode,  # sandbox or live 
+        "mode": 'sandbox',  # sandbox or live 
         "client_id": client_id,
         "client_secret": client_secret
     })
