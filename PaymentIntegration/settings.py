413e8c6b0c7df1c875b11c428d251caf36fdb1ad
KC_software_en <kisha.cairncross@gmail.com>
2025-01-24 11:02:33 +0200

    added DATABASES dictionary back into PaymentIntegration/settings.py

--- PaymentIntegration/settings.py
+++ PaymentIntegration/settings.py
@@ -88,16 +88,20 @@ WSGI_APPLICATION = 'PaymentIntegration.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# comment out local database to use Heroku's database when deployed
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 # configure Heroku's database for deployment
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(
+    default = 'sqlite3:///db.sqlite3', # ensure the app works locally if DATABASE URL is not set
+    conn_max_age=600,
+    ssl_require=True
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
